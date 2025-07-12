51dea47c5199761fe91e01051528dbb54ffbfb47
Changkouth Mangij <42955471+bouth7@users.noreply.github.com>
2025-07-12 18:43:52 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-import Navbar from '../components/Navbar'
+import Head from 'next/head'
 
 export default function Home() {
   return (
     <>
-      <Navbar />
-      <main>
+      <Head>
+        <title>SeekWorker</title>
+      </Head>
+      <main style={{ textAlign: 'center', marginTop: '4rem' }}>
         <h1>Welcome to SeekWorker</h1>
         <p>Connect with Skilled Workers, Instantly.</p>
       </main>
